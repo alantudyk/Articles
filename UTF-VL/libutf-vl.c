d7fe227e93fd58a69dee810a234437ff54c831ea
Reznov <78517110+alantudyk@users.noreply.github.com>
2022-08-21 20:55:46 +0300

    >>

--- UTF-VL/libutf-vl.c
+++ UTF-VL/libutf-vl.c
@@ -7,7 +7,7 @@ void  vl_free( str_t *s) {
 
 void vl_rfree(rstr_t *r) {
     free(r->p);
-    free(r->ap);
+    free(r->a);
     *r = (rstr_t){};
 }
 
@@ -19,22 +19,45 @@ _Bool vl_requal(const rstr_t *a, const rstr_t *b) {
     return vl_equal((const str_t *)a, (const str_t *)b);
 }
 
+void  vl_move( str_t *a,  str_t *b) { *b = *a; *a = ( str_t){}; }
+void vl_rmove(rstr_t *a, rstr_t *b) { *b = *a; *a = (rstr_t){}; }
+
+_Bool  vl_clone(const  str_t *a,  str_t *b) {
+    if (a->s == 0) { *b = (str_t){}; return 0; }
+    *b = (str_t){ malloc(a->s) , a->s };
+    if (b->p == NULL) return 1;
+    memcpy(b->p, a->p, b->s);
+    return 0;
+}
+
+_Bool vl_rclone(const rstr_t *a, rstr_t *b) {
+    if (a->s == 0) { *b = (rstr_t){}; return 0; }
+    *b = (rstr_t){
+        malloc(a->s) , a->s,
+        malloc(a->c * sizeof(uint8_t *)), a->c
+    };
+    if (b->p == NULL || b->a == NULL) return 1;
+    memcpy(b->p, a->p, b->s);
+    memcpy(b->a, a->a, b->c * sizeof(uint8_t *));
+    return 0;
+}
+
+_Bool   vl_move_to_rstr( str_t   *_vl, rstr_t *_rstr) {
+    
+    return 0;
+}
+
+ void vl_move_from_rstr(rstr_t *_rstr,  str_t   *_vl) {
+     *_vl = *(str_t *)_rstr; _rstr->p = NULL; vl_rfree(_rstr);
+}
+
 _Bool   vl_clone_to_rstr(const  str_t   *_vl, rstr_t  *_rstr) {
     if (_vl->s == 0) { *_rstr = (rstr_t){}; return 0; }
-    str_t vl = { malloc(_vl->s) , _vl->s };
-    if (vl.p == NULL) return 1;
-    memcpy(vl.p, _vl->p, vl.s);
-    return vl_move_to_rstr(&vl, _rstr);
+    str_t vl; return vl_clone(_vl, &vl) || vl_move_to_rstr(&vl, _rstr);
 }
 
 _Bool vl_clone_from_rstr(const rstr_t *_rstr,  str_t    *_vl) {
     if (_rstr->s == 0) { *_vl = (str_t){}; return 0; }
-    rstr_t r = {
-        malloc(_rstr->s) , _rstr->s,
-        malloc(_rstr->c * sizeof(void *)), _rstr->c
-    };
-    if (r.p == NULL || r.ap == NULL) return 1;
-    memcpy(r.p , _rstr->p , r.s);
-    memcpy(r.ap, _rstr->ap, r.c * sizeof(void *));
-    return vl_move_from_rstr(&r, _vl);
+    rstr_t r; if (vl_rclone(_rstr, &r)) return 1;
+    vl_move_from_rstr(&r, _vl); return 0;
 }
--- UTF-VL/utf-vl.h
+++ UTF-VL/utf-vl.h
@@ -17,14 +17,18 @@ _Static_assert(__SIZEOF_POINTER__ == 8, "");
 
 typedef struct  str_t { uint8_t *p; size_t s; } str_t;
 typedef struct rstr_t {
-    uint8_t *p;
-    size_t   s;
-    void  **ap;
-    size_t   c;
+    uint8_t  *p;
+    size_t    s;
+    uint8_t **a;
+    size_t    c;
 } rstr_t;
+
+_Bool vl_validate(uint8_t *p, size_t s);
 
 void  vl_free( str_t *s);
 void vl_rfree(rstr_t *r);
+
+size_t vl_char_size(uint32_t *c);
 
 _Bool  vl_char_at(const  str_t *s, size_t i, uint32_t *c);
 _Bool vl_rchar_at(const rstr_t *s, size_t i, uint32_t *c);
@@ -37,8 +41,14 @@ _Bool vl_requal(const rstr_t *a, const rstr_t *b);
 _Bool vl_from_8(const str_t  *_8, str_t *_vl);
 _Bool   vl_to_8(const str_t *_vl, str_t  *_8);
 
-_Bool   vl_move_to_rstr( str_t   *_vl, rstr_t  *_rstr);
-_Bool vl_move_from_rstr(rstr_t *_rstr,  str_t    *_vl);
+void  vl_move( str_t *a,  str_t *b);
+void vl_rmove(rstr_t *a, rstr_t *b);
+
+_Bool  vl_clone(const  str_t *a,  str_t *b);
+_Bool vl_rclone(const rstr_t *a, rstr_t *b);
+
+_Bool   vl_move_to_rstr( str_t   *_vl, rstr_t *_rstr);
+ void vl_move_from_rstr(rstr_t *_rstr,  str_t   *_vl);
 
 _Bool   vl_clone_to_rstr(const  str_t   *_vl, rstr_t  *_rstr);
 _Bool vl_clone_from_rstr(const rstr_t *_rstr,  str_t    *_vl);
