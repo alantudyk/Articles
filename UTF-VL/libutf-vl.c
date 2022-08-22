872d5715457312bafffe12e162d1b2e93286fa04
Reznov <78517110+alantudyk@users.noreply.github.com>
2022-08-22 19:27:26 +0300

    >>

--- UTF-VL/libutf-vl.c
+++ UTF-VL/libutf-vl.c
@@ -1,15 +1,12 @@
 #include "utf-vl.h"
 
-void  vl_free( str_t *s) {
-    free(s->p);
-    *s = (str_t){};
+_Bool vl_validate(uint8_t *p, size_t s, size_t *l) {
+    
+    return 0;
 }
 
-void vl_rfree(rstr_t *r) {
-    free(r->p);
-    free(r->a);
-    *r = (rstr_t){};
-}
+void  vl_free( str_t *s) { free(s->p);             *s = ( str_t){}; }
+void vl_rfree(rstr_t *r) { free(r->p); free(r->a); *r = (rstr_t){}; }
 
 size_t vl_char_size(uint32_t *c) {
     uint8_t *p = (uint8_t *)c;
@@ -18,23 +15,25 @@ size_t vl_char_size(uint32_t *c) {
 }
 
 _Bool  vl_char_at(const  str_t *s, size_t _i, uint32_t *_c) {
-    // if (_i >= s->l) return 1;
+    if (_i >= s->l) return 1;
     size_t i = 0; const uint8_t *p = s->p; uint8_t *c = (uint8_t *)_c;
     for (; i < _i; ++i) while (*p++ & 128);
-    for (;;) { *c++ = *p; if ((*p++ & 128) == 0) break; }
+    do *c++ = *p; while (*p++ & 128);
     while (c - (uint8_t *)_c < 4) *c++ = 0;
     return 0;
 }
 
-_Bool vl_rchar_at(const rstr_t *s, size_t i, uint32_t *c) {
-    
+_Bool vl_rchar_at(const rstr_t *r, size_t _i, uint32_t *c) {
+    if (_i >= r->l) return 1;
+    str_t s = { r->p + r->a[_i / 10] , 0, 10 };
+    vl_char_at(&s, _i % 10, c);
     return 0;
 }
 
 _Bool vl_concat(const str_t *a, const str_t *b, str_t *c) {
     if (a->s == 0) return vl_clone(b, c);
     if (b->s == 0) return vl_clone(a, c);
-    *c = (str_t){ malloc(a->s + b->s) , a->s + b->s };
+    *c = (str_t){ malloc(a->s + b->s), a->s + b->s, a->l + b->l };
     if (c->p == NULL) return 1;
     memcpy(c->p       , a->p, a->s);
     memcpy(c->p + a->s, b->p, b->s);
@@ -54,7 +53,7 @@ void vl_rmove(rstr_t *a, rstr_t *b) { *b = *a; *a = (rstr_t){}; }
 
 _Bool  vl_clone(const  str_t *a,  str_t *b) {
     if (a->s == 0) { *b = (str_t){}; return 0; }
-    *b = (str_t){ malloc(a->s) , a->s };
+    *b = *a, b->p = malloc(b->s);
     if (b->p == NULL) return 1;
     memcpy(b->p, a->p, b->s);
     return 0;
@@ -62,32 +61,31 @@ _Bool  vl_clone(const  str_t *a,  str_t *b) {
 
 _Bool vl_rclone(const rstr_t *a, rstr_t *b) {
     if (a->s == 0) { *b = (rstr_t){}; return 0; }
-    *b = (rstr_t){
-        malloc(a->s) , a->s,
-        malloc(a->c * sizeof(uint8_t *)), a->c
-    };
+    *b = *a, b->p = malloc(b->s), b->a = malloc(b->c);
     if (b->p == NULL || b->a == NULL) return 1;
     memcpy(b->p, a->p, b->s);
-    memcpy(b->a, a->a, b->c * sizeof(uint8_t *));
+    memcpy(b->a, a->a, b->c);
     return 0;
 }
 
-_Bool   vl_move_to_rstr( str_t   *_vl, rstr_t *_rstr) {
+_Bool   vl_move_to_rstr( str_t *s, rstr_t *r) {
     
     return 0;
 }
 
- void vl_move_from_rstr(rstr_t *_rstr,  str_t   *_vl) {
-     *_vl = *(str_t *)_rstr; _rstr->p = NULL; vl_rfree(_rstr);
+ void vl_move_from_rstr(rstr_t *r,  str_t *s) {
+     *s = *(str_t *)r; r->p = NULL; vl_rfree(r);
 }
 
-_Bool   vl_clone_to_rstr(const  str_t   *_vl, rstr_t  *_rstr) {
-    if (_vl->s == 0) { *_rstr = (rstr_t){}; return 0; }
-    str_t vl; return vl_clone(_vl, &vl) || vl_move_to_rstr(&vl, _rstr);
+_Bool   vl_clone_to_rstr(const  str_t *s, rstr_t *r) {
+    if (s->s == 0) { *r = (rstr_t){}; return 0; }
+    str_t c; if (vl_clone(s, &c)) return 1;
+    if (vl_move_to_rstr(&c, r)) { vl_free(&c); return 1; }
+    return 0;
 }
 
-_Bool vl_clone_from_rstr(const rstr_t *_rstr,  str_t    *_vl) {
-    if (_rstr->s == 0) { *_vl = (str_t){}; return 0; }
-    rstr_t r; if (vl_rclone(_rstr, &r)) return 1;
-    vl_move_from_rstr(&r, _vl); return 0;
+_Bool vl_clone_from_rstr(const rstr_t *r,  str_t *s) {
+    if (r->s == 0) { *s = (str_t){}; return 0; }
+    rstr_t c = {}; if (vl_clone((const str_t *)r, (str_t *)&c)) return 1;
+    vl_move_from_rstr(&c, s); return 0;
 }
--- UTF-VL/utf-vl.h
+++ UTF-VL/utf-vl.h
@@ -15,15 +15,10 @@ _Static_assert(__SIZEOF_POINTER__ == 8, "");
 #define fin(stop) for (ssize_t i = -1, _stop = stop; ++i < _stop;)
 #define fix(start, stop, increment) for (ssize_t i = start, _stop = stop; i < _stop; i += increment)
 
-typedef struct  str_t { uint8_t *p; size_t s; } str_t;
-typedef struct rstr_t {
-    uint8_t  *p;
-    size_t    s;
-    uint8_t **a;
-    size_t    c;
-} rstr_t;
+typedef struct  str_t { uint8_t *p; size_t s, l       ; }  str_t;
+typedef struct rstr_t { uint8_t *p; size_t s, l, *a, c; } rstr_t;
 
-_Bool vl_validate(uint8_t *p, size_t s);
+_Bool vl_validate(uint8_t *p, size_t s, size_t *l);
 
 void  vl_free( str_t *s);
 void vl_rfree(rstr_t *r);
@@ -31,28 +26,28 @@ void vl_rfree(rstr_t *r);
 size_t vl_char_size(uint32_t *c);
 
 _Bool  vl_char_at(const  str_t *s, size_t i, uint32_t *c);
-_Bool vl_rchar_at(const rstr_t *s, size_t i, uint32_t *c);
+_Bool vl_rchar_at(const rstr_t *r, size_t i, uint32_t *c);
 
-_Bool vl_concat(const str_t *a, const str_t *b, str_t *dest);
+_Bool vl_concat(const str_t *a, const str_t *b, str_t *_dest);
 
 _Bool  vl_equal(const  str_t *a, const  str_t *b);
 _Bool vl_requal(const rstr_t *a, const rstr_t *b);
   int   vl_cmp (const  str_t *a, const  str_t *b);
   int  vl_rcmp (const rstr_t *a, const rstr_t *b);
 
-_Bool vl_from_8(const str_t  *_8, str_t *_vl);
-_Bool   vl_to_8(const str_t *_vl, str_t  *_8);
+_Bool vl_from_8(const str_t *_8, str_t *_s);
+_Bool   vl_to_8(const str_t *_s, str_t *_8);
 
-void  vl_move( str_t *a,  str_t *b);
-void vl_rmove(rstr_t *a, rstr_t *b);
+void  vl_move( str_t *_src,  str_t *_dest);
+void vl_rmove(rstr_t *_src, rstr_t *_dest);
 
-_Bool  vl_clone(const  str_t *a,  str_t *b);
-_Bool vl_rclone(const rstr_t *a, rstr_t *b);
+_Bool  vl_clone(const  str_t *_src,  str_t *_dest);
+_Bool vl_rclone(const rstr_t *_src, rstr_t *_dest);
 
-_Bool   vl_move_to_rstr( str_t   *_vl, rstr_t *_rstr);
- void vl_move_from_rstr(rstr_t *_rstr,  str_t   *_vl);
+_Bool   vl_move_to_rstr( str_t *s, rstr_t *r);
+ void vl_move_from_rstr(rstr_t *r,  str_t *s);
 
-_Bool   vl_clone_to_rstr(const  str_t   *_vl, rstr_t  *_rstr);
-_Bool vl_clone_from_rstr(const rstr_t *_rstr,  str_t    *_vl);
+_Bool   vl_clone_to_rstr(const  str_t *s, rstr_t *r);
+_Bool vl_clone_from_rstr(const rstr_t *r,  str_t *s);
 
 #endif
