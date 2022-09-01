bed7e8eceb360a6f5201d693d5d232658fc48539
Reznov <78517110+alantudyk@users.noreply.github.com>
2022-09-02 02:45:52 +0300

    >>

--- UTF-VL/libutf-vl.c
+++ UTF-VL/libutf-vl.c
@@ -12,12 +12,8 @@
 #define fiX(iter_name, start, stop, increment) \
     for (ssize_t iter_name = start, _stop = stop; iter_name < _stop; iter_name += increment)
 
-void  vl_init(vl_iterator_t *i, const  str_t *s) {
+ void vl_init(vl_iterator_t *i, const str_t *s) {
     *i = (vl_iterator_t){ s->p, s->l };
-}
-
-void vl_rinit(vl_iterator_t *i, const rstr_t *r) {
-    vl_init(i, (const str_t *)r);
 }
 
 _Bool vl_next(vl_iterator_t *i, int32_t *_c) {
@@ -92,8 +88,7 @@ _Bool   vl_from_bytes(const uint8_t *p, size_t s,  str_t *_dest, size_t *tail) {
     return 0;
 }
 
-void  vl_free( str_t *s) { free(s->p);             *s = ( str_t){}; }
-void vl_rfree(rstr_t *r) { free(r->p); free(r->a); *r = (rstr_t){}; }
+void vl_free(str_t *s) { free(s->p); *s = (str_t){}; }
 
 size_t vl_char_size(const int32_t *c) {
     const uint8_t *p = (const uint8_t *)c;
@@ -133,15 +128,11 @@ _Bool vl_concat(const str_t *a, const str_t *b, str_t *c) {
     return 0;
 }
 
-_Bool  vl_equal(const  str_t *a, const  str_t *b) {
+_Bool vl_equal(const str_t *a, const str_t *b) {
     return a == b || (a->s == b->s && a->l == b->l && !memcmp(a->p, b->p, a->s));
 }
 
-_Bool vl_requal(const rstr_t *a, const rstr_t *b) {
-    return vl_equal((const str_t *)a, (const str_t *)b);
-}
-
-int  vl_cmp(const  str_t *a, const  str_t *b) {
+ int   vl_cmp (const str_t *a, const str_t *b) {
     if (a->l > b->l) { const void *t = a; a = b, b = t; }
     const uint8_t *pa = a->p, *pb = b->p;
     if (a->l == a->s && b->l == b->s)
@@ -155,10 +146,6 @@ int  vl_cmp(const  str_t *a, const  str_t *b) {
             if (ca != cb) return ca - cb;
         }
     return -(b->l > a->l);
-}
-
-int vl_rcmp(const rstr_t *a, const rstr_t *b) {
-    return vl_cmp((const str_t *)a, (const str_t *)b);
 }
 
 _Bool vl_from_8(const junk_t *_8,  str_t *_s) {
@@ -203,53 +190,12 @@ _Bool   vl_to_8(const  str_t *_s, junk_t *_8) {
     return 0;
 }
 
-void  vl_move( str_t *a,  str_t *b) { *b = *a; *a = ( str_t){}; }
-void vl_rmove(rstr_t *a, rstr_t *b) { *b = *a; *a = (rstr_t){}; }
+void vl_move(str_t *a, str_t *b) { *b = *a; *a = (str_t){}; }
 
-_Bool  vl_clone(const  str_t *a,  str_t *b) {
+_Bool vl_clone(const str_t *a, str_t *b) {
     if (a->s == 0) { *b = (str_t){}; return 0; }
     *b = *a, b->p = malloc(b->s);
     if (b->p == NULL) return 1;
     memcpy(b->p, a->p, b->s);
     return 0;
-}
-
-_Bool vl_rclone(const rstr_t *a, rstr_t *b) {
-    if (a->s == 0) { *b = (rstr_t){}; return 0; }
-    const size_t z = (a->l / 16 + 1) * 4 * (1 + (a->s > (1L << 32)));
-    *b = *a, b->p = malloc(b->s), b->a = malloc(z);
-    if (b->p == NULL || b->a == NULL) return 1;
-    memcpy(b->p, a->p, b->s);
-    memcpy(b->a, a->a,    z);
-    return 0;
-}
-
-_Bool   vl_move_to_rstr( str_t *s, rstr_t *r) {
-    if (s->s == 0) { *r = (rstr_t){}; return 0; }
-    _Bool Z = s->s > (1L << 32);
-    if ((r->a = malloc((s->l / 16 + 1) * 4 * (1 + Z))) == NULL) return 1;
-    *(str_t *)r = *s; *s = (str_t){};
-    const uint8_t *p = r->p, *const P = p + r->s;
-    fiN(j, r->l / 16 + 1) {
-        if (Z) ((uint64_t *)r->a)[j] = p - r->p; else ((uint32_t *)r->a)[j] = p - r->p;
-        fiN(_, 16) { while (*p++ & 128); if (p == P) break; }
-    }
-    return 0;
-}
-
- void vl_move_from_rstr(rstr_t *r,  str_t *s) {
-     *s = *(str_t *)r; r->p = NULL; vl_rfree(r);
-}
-
-_Bool   vl_clone_to_rstr(const  str_t *s, rstr_t *r) {
-    if (s->s == 0) { *r = (rstr_t){}; return 0; }
-    str_t c; if (vl_clone(s, &c)) return 1;
-    if (vl_move_to_rstr(&c, r)) { vl_free(&c); return 1; }
-    return 0;
-}
-
-_Bool vl_clone_from_rstr(const rstr_t *r,  str_t *s) {
-    if (r->s == 0) { *s = (str_t){}; return 0; }
-    rstr_t c = {}; if (vl_clone((const str_t *)r, (str_t *)&c)) return 1;
-    vl_move_from_rstr(&c, s); return 0;
 }
--- UTF-VL/utf-vl.h
+++ UTF-VL/utf-vl.h
@@ -11,15 +11,13 @@ typedef struct rstr_t { uint8_t *p; size_t s, l; void *a; } rstr_t;
 
 typedef struct vl_iterator_t { uint8_t *p; size_t l; } vl_iterator_t;
 
- void  vl_init(vl_iterator_t *iterator, const  str_t *s);
- void vl_rinit(vl_iterator_t *iterator, const rstr_t *r);
-_Bool  vl_next(vl_iterator_t *iterator,      int32_t *c);
+ void vl_init(vl_iterator_t *iterator, const  str_t *s);
+_Bool vl_next(vl_iterator_t *iterator,      int32_t *c);
 
 _Bool vl_8_from_bytes(const uint8_t *p, size_t s, junk_t *_dest, size_t *tail);
 _Bool   vl_from_bytes(const uint8_t *p, size_t s,  str_t *_dest, size_t *tail);
 
-void  vl_free( str_t *s);
-void vl_rfree(rstr_t *r);
+void vl_free(str_t *s);
 
 size_t vl_char_size(const int32_t *c);
 
@@ -28,24 +26,14 @@ _Bool vl_rchar_at(const rstr_t *r, size_t i, int32_t *c);
 
 _Bool vl_concat(const str_t *a, const str_t *b, str_t *_dest);
 
-_Bool  vl_equal(const  str_t *a, const  str_t *b);
-_Bool vl_requal(const rstr_t *a, const rstr_t *b);
-  int   vl_cmp (const  str_t *a, const  str_t *b);
-  int  vl_rcmp (const rstr_t *a, const rstr_t *b);
+_Bool  vl_equal(const str_t *a, const str_t *b);
+  int   vl_cmp (const str_t *a, const str_t *b);
 
 _Bool vl_from_8(const junk_t *_8,  str_t *_s);
 _Bool   vl_to_8(const  str_t *_s, junk_t *_8);
 
-void  vl_move( str_t *_src,  str_t *_dest);
-void vl_rmove(rstr_t *_src, rstr_t *_dest);
+void vl_move(str_t *_src, str_t *_dest);
 
-_Bool  vl_clone(const  str_t *_src,  str_t *_dest);
-_Bool vl_rclone(const rstr_t *_src, rstr_t *_dest);
-
-_Bool   vl_move_to_rstr( str_t *s, rstr_t *r);
- void vl_move_from_rstr(rstr_t *r,  str_t *s);
-
-_Bool   vl_clone_to_rstr(const  str_t *s, rstr_t *r);
-_Bool vl_clone_from_rstr(const rstr_t *r,  str_t *s);
+_Bool vl_clone(const str_t *a, str_t *b);
 
 #endif
