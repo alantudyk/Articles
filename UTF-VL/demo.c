e43f2683da77739e2711d06fb8fa7c5588e40382
Reznov <78517110+alantudyk@users.noreply.github.com>
2022-09-03 06:55:42 +0300

    >>

--- UTF-VL/demo.c
+++ UTF-VL/demo.c
@@ -6,6 +6,8 @@
 #define pf printf
 #define fin(stop) for (ssize_t i = -1, _stop = stop; ++i < _stop;)
 
+#define S8 "        "
+
 int cmp(void *a, void *b) { return vl_cmp(*(const str_t **)a, *(const str_t **)b); }
 
 int main(void) {
@@ -18,13 +20,13 @@ int main(void) {
     const size_t jz = sizeof(j8) / 8; str_t s[jz];
     fin(jz) if (vl_from_8(&(junk_t){ j8[i], strlen(j8[i]) }, s + i)) return 1;
     
-    pf("\n\tPrinting two strings vertically using `vl_iterator_t`:\n\n");
+    pf("\n" S8 "Printing two strings vertically using `vl_iterator_t`:\n\n");
     
     vl_iterator_t iter[2]; fin(2) vl_init(iter + i, s + i); int32_t c[2];
     
     fin(s[0].l > s[1].l ? s[0].l : s[1].l) {
         fin(2) if (vl_next(iter + i, c + i)) c[i] = ' ';
-        pf("\t\t"); pc(c[0]); pf("\t"); pc(c[1]); pf("\n");
+        pf(S8 S8); pc(c[0]); pf(S8); pc(c[1]); pf("\n");
     }
     
     pf("\n");
